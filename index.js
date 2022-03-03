cd9ff09fc8168bbad7a5ef3ed7e7d5142637b555
Sadie <sadie@Sadies-MacBook-Pro.local>
2022-03-03 14:40:00 -0800

    commit tree 3241711db9dc6111a5ef8e25a17b56d0500a75a9

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a +b;
+}
+function subtract(a, b) {
+    return "something";
+}
+function subtract (a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return "something";
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return "something";
+}
+function divide (a, b) {
+    return a / b;
+}
